0149473b25e765c513476483ec171103aaf9998e
Agustin Cantero <cantero.agustin@yahoo.com.ar>
2023-06-08 23:49:40 -0300

    cambios

--- pages/login/LoginPage.js
+++ pages/login/LoginPage.js
@@ -1,18 +1,43 @@
-import { useEffect, useContext } from 'react'
-import { StyleSheet, View } from 'react-native';
+import { useEffect, useContext, useState } from 'react'
+import { StyleSheet, View, Modal, Pressable, Text, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import UserAvatar from 'react-native-user-avatar';
+import { useForm, Controller } from "react-hook-form";
 
 import { StatusBar } from 'expo-status-bar';
 import * as WebBrowser from 'expo-web-browser'
 import * as Google from 'expo-auth-session/providers/google'
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import Input from '@mui/material/Input';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import { FormControl } from '@mui/material';
+
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { AuthenticatedUserContext } from '../../navigation/AuthenticatedUserProvider';
 
 WebBrowser.maybeCompleteAuthSession();
 
+const schema = yup.object({
+  apodo: yup.string().required(),
+}).required();
+
 export default function LoginPage() {
   const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [tempUser, setTempUser] = useState(null);
+  const [posicion, setPosicion] = useState('');
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const handleChange = (event) => {
+    setPosition(event.target.value);
+  };
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: "535761942486-oas8lvtva2m7j08qb5nhq0n1uk97kh1j.apps.googleusercontent.com",
@@ -30,7 +55,6 @@ export default function LoginPage() {
       if (response?.type === "success") {
         await getUserInfo(response.authentication.accessToken);
       }
-
     } else {
       setUser(user);
     }
@@ -45,13 +69,19 @@ export default function LoginPage() {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-
       const user = await response.json();
+      setTempUser(user);
       await AsyncStorage.setItem('@user', JSON.stringify(user));
-      setUser(user);
+      setModalVisible(true);
     } catch (error) {
 
     }
+  };
+
+  const handleSubmitModal = data => {
+    console.log(data);
+    setUser(tempUser);
+    setModalVisible(!modalVisible)
   };
 
   return (
@@ -62,7 +92,54 @@ export default function LoginPage() {
         Login with Google
       </FontAwesome.Button>}
 
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {tempUser && <UserAvatar size={100} src={tempUser.picture} />}
+            <Text style={styles.text}>Completa tu perfil de jugador.</Text>
+            <form style={{ height: '100%' }} onSubmit={handleSubmit(handleSubmitModal)}>
+              <Controller
+                name="apodo"
+                control={control}
+                render={({ field }) => <Input {...field} />}
+              />
+              <p>{errors.apodo?.message}</p>
 
+              <Controller
+                name="select"
+                control={control}
+                render={({ field }) => <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="demo-controlled-open-select-label">Posicion</InputLabel>
+                  <Select
+                    {...field}
+                    labelId="demo-controlled-open-select-label"
+                    id="demo-controlled-open-select"
+                    value={posicion}
+                    label="Posicion *"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={'arquero'}>Arquero</MenuItem>
+                    <MenuItem value={'defensor'}>Defensor</MenuItem>
+                    <MenuItem value={'medio'}>Medio</MenuItem>
+                    <MenuItem value={'delantero'}>Delantero</MenuItem>
+                  </Select>
+                </FormControl>
+                }
+              />
+              <input type="submit" />
+            </form>
+          </View>
+        </View>
+      </Modal>
 
 
       <StatusBar style="auto" />
@@ -76,5 +153,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalView: {
+    margin: 20,
+    height: 500,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  image: {
+    marginTop: 150,
+    marginBottom: 10,
+    width: '100%',
+    height: 350,
   },
 });
